f6a784049c56165c2149d072bd6ba694597b3546
tonnylyz <lyztonny@gmail.com>
2019-03-10 23:52:12 +0800

    Spinlock seems work

--- boot/main.c
+++ boot/main.c
@@ -33,9 +33,9 @@ volatile static u32 counter = 0;
 
 int main() {
     core_id = CPURegisterDNUM();
-    mmio_write(0x01840040, 0); // disable l1d cache
-    mmio_write(0x01845044, 1); // l1d cache write back invalid
-    //xmc_mem_remap();
+    //mmio_write(0x01840040, 0); // disable l1d cache
+    //mmio_write(0x01845044, 1); // l1d cache write back invalid
+
     if (core_id == 0) {
         printf("DSP_1 OS Build: %s %s\n", __DATE__, __TIME__);
 
@@ -58,59 +58,13 @@ int main() {
         partition_add(&p2_conf);
         partition_add(&p3_conf);
 
-        printf("p0 @core %d\n", pcb_list[0].target_core);
-        printf("p1 @core %d\n", pcb_list[1].target_core);
-        printf("p2 @core %d\n", pcb_list[2].target_core);
-        printf("p3 @core %d\n", pcb_list[3].target_core);
-        printf("ready to partition_start\n");
-
         dsp2_start_core();
-
-        spinlock_init();
-
-        spinlock_lock(4);
-        printf("counter init: %x\n", counter);
-        spinlock_unlock(4);
-
-        int i;
-        for (i = 0; i < 0x1000000; i++) {
-            spinlock_lock(4);
-            counter++;
-            xmc_invalidate_buffer();
-            spinlock_unlock(4);
-        }
-
-        spinlock_lock(4);
-        printf("counter end: %x\n", counter);
-        spinlock_unlock(4);
 
         partition_start();
 
 
     } else {
         printf("DSP_2 OS Build: %s %s\n", __DATE__, __TIME__);
-
-        spinlock_init();
-
-        spinlock_lock(4);
-        printf("counter init: %x\n", counter);
-        spinlock_unlock(4);
-
-        int i;
-        for (i = 0; i < 0x1000000; i++) {
-            spinlock_lock(4);
-            counter++;
-            xmc_invalidate_buffer();
-            spinlock_unlock(4);
-        }
-
-        spinlock_lock(4);
-        printf("counter end: %x\n", counter);
-        spinlock_unlock(4);
-
-        while (1) {
-            __asm ("\tNOP");
-        }
 
         intc_init();
         printf("intc_init done\n");
--- platform/csp/spinlock.h
+++ platform/csp/spinlock.h
@@ -3,9 +3,7 @@
 
 #include "types.h"
 
-void spinlock_init();
 void spinlock_lock(u8 id);
-int spinlock_try_lock(u8 id);
 void spinlock_unlock(u8 id);
 
 #endif //UCOS2_C66X_SPINLOCK_H
