ee0545a9179aa92627e1251660b15a309846b343
tonnylyz <lyztonny@gmail.com>
2019-03-23 23:12:41 +0800

    Add APEX typedef

--- klib/apex.c
+++ klib/apex.c
@@ -3,6 +3,7 @@
 //
 
 #include "apex.h"
+#include "printf.h"
 #include <partition.h>
 
 return_code_t apex_set_partition_mode(operating_mode_t ps) {
@@ -18,15 +19,9 @@ return_code_t apex_set_partition_mode(operating_mode_t ps) {
     partition_current->operating_mode = ps;
     if (ps == opm_idle) {
         // shutdown the partition
-    } else if (ps == opm_warm_start || ps == opm_cold_start) {
-        // inhibit process scheduling and switch back to initialization mode
-    } else /*if (ps == opm_normal)*/ {
-        /*set first release points of all previously started periodic processes
-        to their next partition period;
-        set first release points of all previously delay started periodic processes
-        to their next partition period, including their delay times;
-        calculate the DEADLINE_TIME of all non-dormant processes in the partition;
-        activate the process scheduling;*/
+        partition_current->target_core = 255u;
+    } else {
+        printf("Warning: setting partition_mode except for idle mode deal no change to the system");
     }
     return r_no_error;
 }
--- klib/def.h
+++ klib/def.h
@@ -5,6 +5,8 @@
 #ifndef UCOS2_C66X_DEF_H
 #define UCOS2_C66X_DEF_H
 
+#include <types.h>
+
 typedef enum {
     sc_normal_start,
     sc_partition_restart,
@@ -33,5 +35,65 @@ typedef enum {
     pt_system,
     pt_normal,
 } partition_type_t;
+
+typedef u32 system_time_t;
+
+#define PORT_MAX_NUMBER 128
+
+typedef u32 message_addr_t;
+typedef u32 message_size_t;
+
+typedef enum {
+    pd_source,
+    pd_destination,
+} port_direction_t;
+
+typedef u32 sampling_port_id_t;
+typedef char* sampling_port_name_t;
+typedef enum {
+    v_invalid,
+    v_valid,
+} validity_t;
+
+typedef struct {
+    u32 max_message_size;
+    port_direction_t port_direction;
+    system_time_t refresh_period;
+    validity_t last_msg_validity;
+} sampling_port_status_t;
+
+typedef struct {
+    sampling_port_id_t id;
+    sampling_port_status_t status;
+} sampling_port_t;
+
+typedef u32 queuing_port_id_t;
+typedef char* queuing_port_name_t;
+
+typedef enum {
+    qd_fifo,
+    qd_priority,
+} queuing_discipline_t;
+
+typedef u32 message_range_t;
+typedef u32 waiting_range_t;
+
+typedef struct {
+    message_range_t nb_message;
+    message_range_t max_nb_message;
+    message_size_t max_message_size;
+    port_direction_t port_direction;
+    waiting_range_t waiting_processes;
+} queuing_port_status_t;
+
+
+
+return_code_t apex_create_sampling_port(
+        sampling_port_name_t sampling_port_name,
+        message_size_t max_message_size,
+        port_direction_t port_direction,
+        system_time_t refresh_period,
+        sampling_port_id_t *p_sampling_port_id);
+
 
 #endif //UCOS2_C66X_DEF_H
