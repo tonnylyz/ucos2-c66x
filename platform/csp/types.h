b47ac288524ea05ab78de052f95dff834790faba
tonnylyz <lyztonny@gmail.com>
2019-03-23 23:11:09 +0800

    Add bool type

--- platform/csp/types.h
+++ platform/csp/types.h
@@ -14,6 +14,11 @@ typedef unsigned int u32;
 typedef unsigned short u16;
 typedef unsigned char u8;
 
+typedef u8 bool;
+#define true 1
+#define false 0
+
+
 #define NULL ((void *)0)
 
 #endif /* TYPES_H_ */
